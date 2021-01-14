e5b3c7b2e654241db5de8117d8b9dfd064086646
rupanshusoi <rupanshusoi@gmail.com>
2021-01-14 23:24:22 +0530

    Fix basic parser

--- lua/scheme.lua
+++ lua/scheme.lua
@@ -1,102 +1,66 @@
-function eval_arithmetic(expr)
+local function eval_arithmetic(expr)
   if expr.func == 'add1' then
     return expr.arg1 + expr.arg2
   end
 end
 
---[[
-function parse(tokens)
-  assert(#tokens > 0)
-  if tokens[1] == '(' then
-    assert(tokens[#tokens] == ')')
-
-    tokens_trimmed = { table.unpack(tokens, 2, #tokens - 1) }
-    expr = {}
-
-    idx = 1
-    while idx <= #tokens_trimmed do
-      if idx == 1 then
-        expr.func = tokens_trimmed[idx]
-        idx = idx + 1
-      else
-        if tokens_trimmed[idx] == '(' then
-          idx_closep = 0
-          for i = idx + 1, #tokens_trimmed do
-            if tokens_trimmed[i] == ')' then
-              idx_closep = i
-              break
-            end
-          end
-          assert(idx_closep ~= 0)
-          expr['arg' .. tostring(idx - 1)] = parse({ table.unpack(tokens_trimmed, idx, idx_closep) })
-          idx = idx_closep + 1
-        else
-          expr['arg' .. tostring(idx - 1)] = tonumber(tokens_trimmed[idx])
-          idx = idx + 1
-        end
-      end
-    end
-    return expr
-  else
-    assert(#tokens == 1)
-    return tonumber(tokens[1])
-  end
-end
---]]
-    
-function parse(tokens)
-  print 'parse called'
+local function parse(tokens)
   assert(#tokens > 2)
   assert(tokens[1] == '(')
   assert(tokens[#tokens] == ')')
 
-  expr = {}
+  local expr = {}
   expr.func = tokens[2]
 
   -- Remove all tokens except args
-  tokens_trimmed = { table.unpack(tokens, 3, #tokens - 1) }
+  local tokens_trimmed = { table.unpack(tokens, 3, #tokens - 1) }
 
-  i = 1
+  local i, argc = 1, 1
   while i <= #tokens_trimmed do
     if tokens_trimmed[i] == '(' then
-      j = i
+      local j, paren = i + 1, 0
       while j < #tokens_trimmed do
-        if tokens_trimmed[j] == '(' then
-          break
+        if tokens_trimmed[j] == ')' then
+          if paren == 0 then
+            break
+          end
+          paren = paren - 1
+        elseif tokens_trimmed[j] == '(' then
+          paren = paren + 1
         end
         j = j + 1
       end
-      expr['arg' .. tostring(i)] = parse({ table.unpack(tokens_trimmed, i, j) })
+      expr['arg' .. tostring(argc)] = parse({ table.unpack(tokens_trimmed, i, j) })
       i = j + 1
     else
-      expr['arg' .. tostring(i)] = tonumber(tokens_trimmed[i])
+      expr['arg' .. tostring(argc)] = tonumber(tokens_trimmed[i])
       i = i + 1
     end
+    argc = argc + 1
   end
 
   return expr
 end
 
-function tokenize(S)
+local function tokenize(S)
   S, _ = S:gsub('%(', ' ( ')
   S, _ = S:gsub('%)', ' ) ')
-  tokens = {}
+  local tokens = {}
   for i in S:gmatch('%S+') do
     table.insert(tokens, i)
   end
   return tokens
 end
 
-function meaning(S, env)
-  expr = parse(tokenize(S))
-  for k,v in pairs(expr) do
-    print(k .. ' ' .. tostring(v))
-  end
+local function meaning(S, env)
+  local expr = parse(tokenize(S))
+
+  print(expr.arg1.arg1.arg2)
 end
 
-function evaluate(S)
+local function evaluate(S)
   return meaning(S, {})
 end
 
 --evaluate('(+ 2 3 4 5)')
-evaluate('(+ 2 (+ 4 5))')
+evaluate('(+ (- (+ 5 6) 4) (+ 1 2))')
