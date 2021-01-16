d1df88c659a0719c4d53763555b94af21831f664
rupanshusoi <rupanshusoi@gmail.com>
2021-01-16 23:37:58 +0530

    Add type AstAtom, support for quote, improve parser

--- lua/scheme.lua
+++ lua/scheme.lua
@@ -1,6 +1,7 @@
 -- Add types
 local AstPrimitive = { __type = 'AstPrimitive' }
 local AstList      = { __type = 'AstList'      }
+local AstAtom      = { __type = 'AstAtom'      }
 
 function AstPrimitive:new(o)
   o = o or { args = {} }
@@ -11,6 +12,13 @@ end
 
 function AstList:new(o)
   o = o or { values = {} }
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+function AstAtom:new(o)
+  o = o or {}
   setmetatable(o, self)
   self.__index = self
   return o
@@ -28,23 +36,31 @@ local function apply_primitive(ast)
   elseif ast.func == 'car' then
     return evaluate(ast.args[1]).values[1]
   else
-    assert(false)
+    assert(false, 'unknown primitive function: ' .. tostring(ast.func))
   end
 end
 
-local function apply_const(ast)
-  return ast.value
+local function find_closep(tokens, openp)
+  local idx, paren = openp + 1, 0
+  while idx < #tokens do
+    if tokens[idx] == ')' then
+      if paren == 0 then break end
+      paren = paren - 1
+    elseif tokens[idx] == '(' then
+      paren = paren + 1
+    end
+    idx = idx + 1
+  end
+  return idx
 end
 
-local function parse(tokens)
+local function parse(tokens, is_quoted)
   assert(#tokens > 2, 'S-expression too small')
-  assert(tokens[1] == '(', 'expected LPAREN but got ' .. tokens[1])
-  assert(tokens[#tokens] == ')', 'expected RPAREN but got ' .. tokens[#tokens])
+  assert(tokens[1] == '(', 'expected LPAREN but got: ' .. tokens[1])
+  assert(tokens[#tokens] == ')', 'expected RPAREN but got: ' .. tokens[#tokens])
 
-  -- Check the first token after LPAREN
-  local is_list = tonumber(tokens[2])
-
-  if is_list then
+  if is_quoted then
+    -- Parse as a list if quoted
     local ast = AstList:new()
 
     -- Remove parens
@@ -55,58 +71,48 @@ local function parse(tokens)
   
       if tokens_trimmed[i] == '(' then
   
-        local j, paren = i + 1, 0
-        while j < #tokens_trimmed do
-          if tokens_trimmed[j] == ')' then
-            if paren == 0 then break end
-            paren = paren - 1
-          elseif tokens_trimmed[j] == '(' then
-            paren = paren + 1
-          end
-          j = j + 1
-        end
-  
-        table.insert(ast.values, parse({ table.unpack(tokens_trimmed, i, j) }))
-        i = j + 1
+        local closep = find_closep(tokens_trimmed, i)
+        table.insert(ast.values, parse({ table.unpack(tokens_trimmed, i, closep) }, is_quoted))
+        i = closep + 1
   
       else
-        table.insert(ast.values, tonumber(tokens_trimmed[i]))
+        table.insert(ast.values, AstAtom:new({ value = tokens_trimmed[i] }))
         i = i + 1
       end
     end
     return ast
   else
-    local ast = AstPrimitive:new()
-    ast.func = tokens[2]
-  
-    -- Remove all tokens except args
+    -- Remove func and outermost parens
     local tokens_trimmed = { table.unpack(tokens, 3, #tokens - 1) }
   
-    local i = 1
-    while i <= #tokens_trimmed do
-  
-      if tokens_trimmed[i] == '(' then
-  
-        local j, paren = i + 1, 0
-        while j < #tokens_trimmed do
-          if tokens_trimmed[j] == ')' then
-            if paren == 0 then break end
-            paren = paren - 1
-          elseif tokens_trimmed[j] == '(' then
-            paren = paren + 1
-          end
-          j = j + 1
+    if tokens[2] == 'quote' then
+      -- Parse as a list
+      return parse(tokens_trimmed, true) 
+    else
+      -- Parse as a (primitive) function application
+      local ast = AstPrimitive:new()
+      ast.func = tokens[2]
+    
+      local i = 1
+      while i <= #tokens_trimmed do
+    
+        if tokens_trimmed[i] == '(' then
+    
+          local closep = find_closep(tokens_trimmed, i)
+          table.insert(ast.args, parse({ table.unpack(tokens_trimmed, i, closep) }))
+          i = closep + 1
+    
+        elseif tonumber(tokens_trimmed[i]) then
+          table.insert(ast.args, tonumber(tokens_trimmed[i]))
+          i = i + 1
+        else
+          -- Must be an atom
+          table.insert(ast.args, AstAtom:new({ value = tokens_trimmed[i] }))
+          i = i + 1
         end
-  
-        table.insert(ast.args, parse({ table.unpack(tokens_trimmed, i, j) }))
-        i = j + 1
-  
-      else
-        table.insert(ast.args, tonumber(tokens_trimmed[i]))
-        i = i + 1
       end
+      return ast
     end
-    return ast
   end
 end
 
@@ -128,6 +134,8 @@ function evaluate(ast)
     return apply_primitive(ast)
   elseif ast.__type == 'AstList' then
     return ast
+  elseif ast.__type == 'AstAtom' then
+    return ast.value
   else
     assert(false)
   end
@@ -145,13 +153,17 @@ function my_print(ast)
     for _, value in pairs(ast.values) do
       my_print(value)
     end
-    io.write(')\n')
+    io.write(') ')
+  elseif ast.__type == 'AstAtom' then
+    io.write(ast.value .. ' ')
   else
     assert(false)
   end
 end
 
-my_print(interpret('(1 2 3)'))
+--my_print(interpret('(quote (quote (1 2 3)))'))
 --my_print(interpret('(* (+ (// (- 16 2) 2) 3) 3)'))
+--my_print(interpret('(+ 2 (+ (* 2 3) 5))'))
 --my_print(interpret('(car (1 2 3))'))
 --my_print(interpret('(car (1 (+ 2 3)))'))
+my_print(interpret('(car (quote ((quote tears bears cries) bears pears)))'))
