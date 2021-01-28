b90190ac7ca228c81482f54896185dcac960fc98
rupanshusoi <rupanshusoi@gmail.com>
2021-01-28 16:14:27 +0530

    Support loading and executing a file passed as a command line arg

--- lua/repl.lua
+++ lua/repl.lua
@@ -1,12 +1,39 @@
+local parser = require('parser')
 local misty = require('misty')
 
 local global_env = {}
 
-while true do
-  io.write('Misty$ ')
-  local line = io.read()
-  local value = misty.interpret(line, global_env)
-  if value then
-    misty.my_printn(value)
+if arg[1] then
+  local file = io.open(arg[1])
+  
+  local program = file:read('*all')
+  local tokens = parser.expand_quotes(parser.tokenize(program))
+  
+  local global_env = {}
+  
+  local i = 1
+  while i < #tokens do
+    local closep = parser.find_closep(tokens, i)
+  
+    local parse_tree = parser.main(parser.parse_list{ table.unpack(tokens, i, closep) })
+    local value = misty.evaluate(parse_tree, global_env)
+    if value then
+      misty.my_printn(value)
+    end
+  
+    i = closep + 1
   end
+  
+  file:close()
+
+else
+  while true do
+    io.write('Misty$ ')
+    local line = io.read()
+    local value = misty.interpret(line, global_env)
+    if value then
+      misty.my_printn(value)
+    end
+  end
+
 end
